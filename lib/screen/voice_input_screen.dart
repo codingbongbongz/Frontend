4f0f6071a02ce18654941881e7afd47e6cc1abd1
durumi99 <kms990615@naver.com>
2023-08-02 14:58:45 +0900

    [refactor] class 분리

--- lib/screen/voice_input_screen.dart
+++ lib/screen/voice_input_screen.dart
@@ -45,24 +45,6 @@ class _VoiceInputScreenState extends State<VoiceInputScreen> {
       final bytes = await file.readAsBytes();
       // print(file);
       log(bytes.toString());
-      // File audioFile = File(audioPath);
-      // final bytes = audioFile.readAsBytesSync();
-      // final byteData = bytes.buffer.asByteData();
-      // if (kDebugMode) {
-      //   print(audioFile); // print(content);
-      //   print(bytes);
-      //   print(byteData);
-      // }
-
-      // FilePickerResult? result =
-      //     await FilePicker.platform.pickFiles(initialDirectory: audioPath);
-      // if (result != null) {
-      //   print(result.files.first.toString());
-      //   // Uint8List? fileBytes = result.files.first.bytes;
-      //   // String fileName = result.files.first.name;
-      //   // print(fileBytes);
-      //   // print(fileName);
-      // }
     } catch (e) {
       if (kDebugMode) {
         print("readFile Error : $e");
@@ -73,18 +55,6 @@ class _VoiceInputScreenState extends State<VoiceInputScreen> {
   Future<void> startRecording() async {
     try {
       if (await audioRecord.hasPermission()) {
-        // 경로 지정을 통한 파일 확장자 지정
-        // String path;
-        // final dir = await getTemporaryDirectory();
-        // path = p.join(
-        //   "file://${dir.path}",
-        //   'audio_${DateTime.now().millisecondsSinceEpoch}.mp4',
-        // );
-
-        // if (kDebugMode) {
-        //   print(path);
-        // }
-        // await audioRecord.start(path: path);
         await audioRecord.start();
 
         setState(() {
@@ -116,7 +86,9 @@ class _VoiceInputScreenState extends State<VoiceInputScreen> {
   Future<void> playRecording() async {
     try {
       Source urlSource = UrlSource(audioPath);
-      print(urlSource);
+      if (kDebugMode) {
+        print(urlSource);
+      }
       await audioPlayer.play(urlSource);
     } catch (e) {
       if (kDebugMode) {
@@ -133,11 +105,16 @@ class _VoiceInputScreenState extends State<VoiceInputScreen> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           if (isRecording)
-            const Text(
-              "Recording in Progress",
-              style: TextStyle(
-                fontSize: 20,
-              ),
+            const Column(
+              children: [
+                CircularProgressIndicator(),
+                Text(
+                  "Recording in Progress",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+              ],
             ),
           ElevatedButton(
             onPressed: isRecording ? stopRecording : startRecording,
@@ -147,15 +124,20 @@ class _VoiceInputScreenState extends State<VoiceInputScreen> {
           const SizedBox(
             height: 25,
           ),
-          ElevatedButton(
-            onPressed: readFile,
-            child: Text('readFile'),
-          ),
           if (!isRecording && audioPath != '')
-            ElevatedButton(
-              onPressed: playRecording,
-              child: Text('Play Recording'),
-            )
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: readFile,
+                  child: Text('submit'),
+                ),
+                ElevatedButton(
+                  onPressed: playRecording,
+                  child: Text('Play Recording'),
+                )
+              ],
+            ),
         ],
       ),
     ));
